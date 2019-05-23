2b8b03db89247d6022a2f2162fce3c739e3822dc
mavismwt <873498174@qq.com>
2019-05-23 14:16:53 +0800

    Recover version5.2.1

--- pages/teacher/info/student/student.js
+++ pages/teacher/info/student/student.js
@@ -4,7 +4,8 @@ var check1 = true;
 var check2 = false;
 var check3 = false;
 var checks = [true, false, false];
-
+var isSelectHidden = true;
+var multiIndex = [[0, 0]];
 Page({
 
   /**
@@ -16,70 +17,19 @@ Page({
     switch2: true,
     value: 'sex',
     ismale: ismale,
-    sexRoc: [{
-      "title": '男',
-      check: check1,
-    },
-    {
-      "title": '女',
-      check: check2,
-    },
-    {
-      "title": '不限',
-      check: check3,
-    }
-    ],
-    disabled: false,
+    sexRoc: [{"title": '男',check: check1,},{"title": '女',check: check2,},{"title": '不限',check: check3,}],
     checks: checks,
     index: 0,
     singleArray: ['一年级', '二年级', '三年级', '四年级', '五年级', '六年级', '初一', '初二', '初三', '高一', '高二', '高三'],
-    multiArray: [['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'], ['上午', '下午', '晚上']],
-    objectMultiArray: [
-      [
-        {
-          id: 0,
-          name: '星期一'
-        },
-        {
-          id: 1,
-          name: '星期二'
-        },
-        {
-          id: 2,
-          name: '星期三'
-        },
-        {
-          id: 3,
-          name: '星期四'
-        },
-        {
-          id: 4,
-          name: '星期五'
-        },
-        {
-          id: 5,
-          name: '星期六'
-        },
-        {
-          id: 6,
-          name: '星期日'
-        }
-      ], [
-        {
-          id: 0,
-          name: '上午'
-        },
-        {
-          id: 1,
-          name: '下午'
-        },
-        {
-          id: 2,
-          name: '晚上'
-        }
-      ]
-    ],
-    multiIndex: [0, 0],
+    multiArray: [
+      ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'], ['上午', '下午', '晚上']],
+    objectMultiArray: [[{id: 0,name: '星期一'}, {id: 1,name: '星期二'},{id: 2,name: '星期三'},{id: 3,name: '星期四'},{id: 4,name: '星期五'},{id: 5,name: '星期六'},{id: 6,name: '星期日'}], 
+      [{id: 0,name: '上午'},{id: 1,name: '下午'},{id: 2,name: '晚上'}]],
+    multiIndex: multiIndex,
+    objectArray: [
+      { object: '语文', isSelected: false }, { object: '数学', isSelected: false }, { object: '英语', isSelected: false }, { object: '物理', isSelected: false }, { object: '化学', isSelected: false }, { object: '生物', isSelected: false }, { object: '政治', isSelected: false }, { object: '历史', isSelected: false }, { object: '地理', isSelected: false }, { object: '其他', isSelected: false }],
+    isSelectHidden: isSelectHidden,
+    timeArray: [{day:'',detail:''}]
   },
 
   onChange1: function (e) {
@@ -92,7 +42,20 @@ Page({
       switch2: e.detail.value
     })
   },
-
+  select: function (e) {
+    let index = e.currentTarget.dataset['index']
+    let status = !e.currentTarget.dataset['status']
+    let urlStr = 'objectArray[' + index + '].isSelected'
+    this.setData({
+      [urlStr]: status,
+    })
+  },
+  changeSelectStatus: function() {
+    isSelectHidden = !isSelectHidden
+    this.setData({
+      isSelectHidden: isSelectHidden
+    })
+  },
   radioChange: function (e) {
     if (this.data.disabled) return;
     ismale = !ismale
@@ -120,6 +83,14 @@ Page({
       checks: checks
     })
   },
+
+  addTime: function(e) {
+    multiIndex.push([0,0])
+    this.setData({
+      multiIndex: multiIndex,
+    })
+  },
+
 
   completeInfo: function(e) {
     wx.navigateTo({
